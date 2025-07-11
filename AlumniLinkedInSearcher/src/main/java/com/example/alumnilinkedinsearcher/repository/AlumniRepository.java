fbf2e5e682245ff18b22b76bebb591ed7feb1258
Ghratika Gupta <ghratikagupta@email.com>
2025-07-11 14:22:40 +0530

    Remove assignment

--- AlumniLinkedInSearcher/src/main/java/com/example/alumnilinkedinsearcher/repository/AlumniRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.alumnilinkedinsearcher.repository;
-
-import com.example.alumnilinkedinsearcher.model.Alumni;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumniRepository extends JpaRepository<Alumni, Long> {}
